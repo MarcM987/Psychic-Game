f2714f0625223093c4e5adc69a5ee411da7b6dd5
Marc_M <marcmartinez987@gmail.com>
2020-04-25 16:55:12 -0700

    Added letterGuess, myWord, and writeWord functions, called after letter press

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,53 @@
-var input = prompt("Do You have a game save");
+// var input = confirm("Do You have a game save");
 
 var game = {
-    name = "HangMan",
-    theme = "Technology",
-    wins = 0,
-    loses = 0,
-    guesses = 6,
-    wordbank = ["quantum","database", "programming", "html", "javascript", "hexadecimal", "binary", "computers"],
-    wordGuess = " ",
+    name: "HangMan",
+    theme: "Technology",
+    wins: 0,
+    loses: 0,
+    guesses: 6,
+    wordBank: ["quantum","database", "programming", "html", "javascript", "hexadecimal", "binary", "computers"],
+    wordGuess: "",
+    word: "",
+    guessCount: 0,
+
+    letterGuess: function(letter){
+        alert("You guessed: " + letter);
+        this.letter = letter;
+        this.myWord(this.letter);
+        ++this.guessCount;
+    },
+
+    myWord: function(letter){
+        if(this.guessCount == 0){
+            word = this.wordBank[Math.floor(Math.random()*this.wordBank.length)];
+        }
+        this.writeWord(word, letter);
+    },
+
+    writeWord: function(word, letter){
+        var wordArry = "";
+        for(let i = 0; i < word.length; ++i){
+            wordArry += " _ ";
+            if(word.indexOf(letter) == i){
+                wordArry[i] = letter;
+            }
+        }
+        alert(word);
+        document.getElementById("Word").innerHTML = wordArry;
+    }
 
 };
 
 var gamesave = {
-    wins = 0,
-    loses = 0,
-    currWordIndex = 0,
+    wins: 0,
+    loses: 0,
+    currWordIndex: 0,
 
 };
+
+document.onkeydown = function(event){
+    var letter = event.key.toLowerCase();
+    game.letterGuess(letter);
+};
+
